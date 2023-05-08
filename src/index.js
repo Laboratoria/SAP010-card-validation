50478599c756e41d92bd348b6e93f419ffdc63fa
Aline Landeira <107163817+alinelandeira@users.noreply.github.com>
2023-05-08 10:32:23 -0300

    alteração do form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
-console.log(validator);
+let btnCard = document.querySelector("#botao");
+let respCard = document.querySelector("#outCard");
+
+btnCard.addEventListener('click', validadorCard);
+
+function validadorCard() {
+    let numCard = document.querySelector("#numDigitado").value;
+
+
