bc4d875f413a429749e92c51433c64f33b143d57
Aline Landeira <107163817+alinelandeira@users.noreply.github.com>
2023-05-15 20:46:23 -0300

    criando aler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import validator from './validator.js';
 
-let btnCard = document.querySelector("#botao");
-let respCard = document.querySelector("#outCard");
+
+const btnCard = document.querySelector("#botao");
+const respCard = document.querySelector("#outCard");
+
 
 btnCard.addEventListener('click', validadorCard);
 
+
 function validadorCard() {
-    let numCard = document.querySelector("#numDigitado").value;
+  const numCard = document.querySelector("#numDigitado").value; 
 
+  const resultMasky = validator.maskify(numCard);
+  
+  const resultadoFinal = validator.isValid(numCard); // argumento
 
+  if (numCard === "") {
+    return respCard.innerText = " Digite o número do cartão"; 
+  }
+
+  else if (resultadoFinal === true) {
+    alert(resultMasky + " Cartão VALIDO! Liberado para Compras")
+  
+  }
+  else {
+      
+    alert(resultMasky + " ERRO Número de cartão Invalido! Favor verificar")
+     
+  } 
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,39 @@
 const validator = {
-  // ...
-};
 
-export default validator;
+  isValid: function (creditCardNumber) {
+    const transfCard = creditCardNumber.split("").reverse();
+    
+    let retCard = 0;
+
+    for (let i = 0; i < transfCard.length; i++) {
+      const numCard = parseInt(transfCard[i])
+
+      if (i % 2 !== 0 && numCard >= 5) {
+        retCard = retCard + (numCard * 2) - 9;
+      }
+
+      else if (i % 2 !== 0 && numCard <= 5) {
+        retCard = retCard + (numCard * 2);
+      }
+      else {
+        retCard = retCard + numCard;
+      }
+
+    }
+    if (retCard % 10 === 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  },
+
+  maskify: function (creditCardNumber){
+  
+    return creditCardNumber.replace(/.(?=.{4})/g, "#")
+     
+  }
+
+}
+
+export default validator; 
